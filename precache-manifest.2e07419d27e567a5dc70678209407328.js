74822fdfbcf6211df30ac3dd4345aaf8464d2a0f
Priyanka Kumari <priyankakumari@gomechanic.in>
2022-07-03 19:12:55 +0530

    Updates

---
+++ precache-manifest.2e07419d27e567a5dc70678209407328.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "61da32eba8cc065d30f9",
+    "url": "/github-repo-assessment/static/css/main.41c8bc24.chunk.css"
+  },
+  {
+    "revision": "61da32eba8cc065d30f9",
+    "url": "/github-repo-assessment/static/js/main.2df22860.chunk.js"
+  },
+  {
+    "revision": "d5bac2976f055977707d",
+    "url": "/github-repo-assessment/static/js/runtime~main.ef63e654.js"
+  },
+  {
+    "revision": "7df5fbc4aae0a77ba9c4",
+    "url": "/github-repo-assessment/static/js/2.62380ce1.chunk.js"
+  },
+  {
+    "revision": "bb895545e484ff0164d4",
+    "url": "/github-repo-assessment/static/js/3.71a42320.chunk.js"
+  },
+  {
+    "revision": "97f863fdce0cede2cffd3c2e8c265691",
+    "url": "/github-repo-assessment/index.html"
+  }
+];
